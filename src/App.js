44c49c1aa8e813a6ed507f259b266aa9c270a334
CamilaSosa <camila_ines_sosa@hotmail.com>
2019-09-12 01:02:49 -0300

    Base del frontend

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../efixlogo.png" className="logo" alt="logo" />
+        <p>
+          Bienvenido a E-FIX!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
