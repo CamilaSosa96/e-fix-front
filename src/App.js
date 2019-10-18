8a3df24486979eeb6e4c172655c5a2fbd55efe9d
CamilaSosa96 <camila_ines_sosa@hotmail.com>
2019-10-18 10:58:18 -0300

    Pantalla para ruta no encontrada

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import HomeScreen from './screens/HomeScreen'
 import LoginScreen from './screens/LoginScreen'
 import NewOrderForm from './screens/NewOrderForm'
 import OrdersScreen from './screens/OrdersScreen'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ApprovalScreen from './screens/ApprovalScreen'
+import NotFoundScreen from './screens/NotFoundScreen'
 
 function App() {
   axios.defaults.withCredentials = true
   return (
     <Router>
+      <Switch>
         <Route exact path='/' component={RedirScreen} />
         <Route exact path='/login' component={LoginScreen} />
         <Route exact path='/home' component={HomeScreen} />
@@ -19,6 +21,8 @@ function App() {
         <Route path='/orders/:searchString' render={props => <OrdersScreen key={Date.now()} {...props} />}/>
         <Route exact path='/orders' component={OrdersScreen} />
         <Route path='/budget/:id/:dni' component={ApprovalScreen}/>
+        <Route component={NotFoundScreen}/>
+      </Switch>
     </Router>
   );
 }
---
+++ src/screens/NotFoundScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class NotFoundScreen extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <p>Página no encontrada! [Volver a home] </p>
+            </div>
+        )
+    }
+
+}
+
+export default NotFoundScreen
