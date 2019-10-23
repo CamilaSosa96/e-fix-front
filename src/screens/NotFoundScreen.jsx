347a679efa81ff0ec1d1c3a49d2f34d5e2d7c9ad
CamilaSosa96 <camila_ines_sosa@hotmail.com>
2019-10-23 12:18:56 -0300

    Mejora en pantalla de ruta inválida

--- src/screens/NotFoundScreen.jsx
+++ src/screens/NotFoundScreen.jsx
@@ -30,14 +30,18 @@ class NotFoundScreen extends React.Component{
                                 position: 'absolute', bottom: '0'}}/>     
                 <div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
                 {this.state.goHome && <Redirect to='/home'/>}
+                <img style={{width: '150px', marginLeft: '15px'}}
+                                            src='http://localhost:3000/brokenlogo.png'
+                                       alt='E-Fix Broken'>
+                           </img>
                 <NonIdealState 
-                                icon='error'
                                 title='No encontrado'
                                 description='La página solicitada no existe'
                                 action={<Button onClick={() => this.setState({goHome: true})}
                                                 icon='home'>
                                             Ir al inicio
                                         </Button>}>
+                                       
                 </NonIdealState>
             </div>
             </div>
