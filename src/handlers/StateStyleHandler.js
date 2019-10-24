93f3ac2c01042a0b92f257674c99da66d7f5043f
CamilaSosa96 <camila_ines_sosa@hotmail.com>
2019-10-24 11:18:34 -0300

    Refactor styleHandlers

--- src/handlers/StateStyleHandler.js
+++ src/handlers/StateStyleHandler.js
@@ -1,64 +1,72 @@
 export function stateNameTranslator(rawStateName){
-    if(rawStateName === 'RECIBIDO')return 'Recibido'
-    if(rawStateName === 'ESPERANDO_PRESUPUESTO')return 'Esperando aprobación de presupuesto'
-    if(rawStateName === 'REPARACION')return 'En reparación'
-    if(rawStateName === 'RETIRAR_SINARREGLO')return 'Listo para retirar sin reparar'
-    if(rawStateName === 'REPARADO')return 'Reparado'
-    if(rawStateName === 'CANCELADA')return 'Reparación cancelada'
-    if(rawStateName === 'ENTREGADO')return 'Entregado'
-    return ''
+    switch(rawStateName){
+        case 'RECIBIDO': return 'Recibido'
+        case 'ESPERANDO_PRESUPUESTO' : return 'Esperando aprobación de presupuesto'
+        case 'REPARACION' : return 'En reparación'
+        case 'RETIRAR_SINARREGLO' : return 'Listo para retirar sin reparar'
+        case 'REPARADO' : return 'Reparado'
+        case 'CANCELADA' : return 'Reparación cancelada'
+        case 'ENTREGADO' : return 'Entregado'
+        default : return ''
+    }
 }
 
 export function stateColorSelector(rawStateName){
-    if(rawStateName === 'RECIBIDO')return '#686868'
-    if(rawStateName === 'ESPERANDO_PRESUPUESTO')return '#22C0EA'
-    if(rawStateName === 'REPARACION')return '#003AFF'
-    if(rawStateName === 'RETIRAR_SINARREGLO')return '#FE0000'
-    if(rawStateName === 'REPARADO')return '#3AFF00'
-    if(rawStateName === 'CANCELADA')return '#9A0000'
-    if(rawStateName === 'ENTREGADO')return '#239A00'
-    return ''
+    switch(rawStateName){
+        case 'RECIBIDO': return '#686868'
+        case 'ESPERANDO_PRESUPUESTO' : return '#22C0EA'
+        case 'REPARACION' : return '#003AFF'
+        case 'RETIRAR_SINARREGLO' : return '#FE0000'
+        case 'REPARADO' : return '#3AFF00'
+        case 'CANCELADA' : return '#9A0000'
+        case 'ENTREGADO' : return '#239A00'
+        default : return ''
+    }
 }
 
 export function stateIconSelector(rawStateName){
-    if(rawStateName === 'RECIBIDO')return 'import'
-    if(rawStateName === 'ESPERANDO_PRESUPUESTO')return 'dollar'
-    if(rawStateName === 'REPARACION')return 'wrench'
-    if(rawStateName === 'RETIRAR_SINARREGLO')return 'ban-circle'
-    if(rawStateName === 'REPARADO')return 'clean'
-    if(rawStateName === 'CANCELADA')return 'delete' 
-    if(rawStateName === 'ENTREGADO')return 'endorsed' 
-    return ''
+    switch(rawStateName){
+        case 'RECIBIDO': return 'import'
+        case 'ESPERANDO_PRESUPUESTO' : return 'dollar'
+        case 'REPARACION' : return 'wrench'
+        case 'RETIRAR_SINARREGLO' : return 'ban-circle'
+        case 'REPARADO' : return 'clean'
+        case 'CANCELADA' : return 'delete' 
+        case 'ENTREGADO' : return 'endorsed'
+        default : return ''
+    }
 }
 
 export function stateTextForClient(rawStateName){
     const text = {msg1: '', msg2: ''}
-    if(rawStateName === 'RECIBIDO'){
-        text.msg1 = 'se encuentra ingresado en el sistema.'
-        text.msg2 = 'Próximamente será revisado por un técnico.'
+    switch(rawStateName){
+        case 'RECIBIDO': 
+            text.msg1 = 'se encuentra ingresado en el sistema.'
+            text.msg2 = 'Próximamente será revisado por un técnico.'
+            return text
+        case 'ESPERANDO_PRESUPUESTO' : 
+            text.msg1 = 'se encuentra esperando la aprobación de la reparación.'
+            return text
+        case 'REPARACION' :
+            text.msg1 = 'se encuentra en proceso de reparación.'
+            text.msg2 = 'Se le notificará cuando termine el mismo.'
+            return text
+        case 'RETIRAR_SINARREGLO' : 
+            text.msg1 = 'se encuentra listo para ser retirado sin reparación.'
+            text.msg2 = 'Esperamos su visita para que lo retire.'
+            return text
+        case 'REPARADO' : 
+            text.msg1 = 'ya se encuentra reparado.'
+            text.msg2 = 'Esperamos su visita para que abone la reparación y retire el producto.'
+            return text
+        case 'CANCELADA' : 
+            text.msg1 = 'ha sido retirado sin reparar.'
+            text.msg2 = 'No se ha aprobado la reparación y esta ha sido cancelada.'
+            return text
+        case 'ENTREGADO' : 
+            text.msg1 = 'ha sido reparado.'
+            text.msg2 = 'La reparación fue abonada y el producto retirado.'
+            return text
+        default : return text
     }
-    if(rawStateName === 'ESPERANDO_PRESUPUESTO'){
-        text.msg1 = 'se encuentra esperando la aprobación de la reparación.'
-    }
-    if(rawStateName === 'REPARACION'){
-        text.msg1 = 'se encuentra en proceso de reparación.'
-        text.msg2 = 'Se le notificará cuando termine el mismo.'
-    }
-    if(rawStateName === 'RETIRAR_SINARREGLO'){
-        text.msg1 = 'se encuentra listo para ser retirado sin reparación.'
-        text.msg2 = 'Esperamos su visita para que lo retire.'
-    }
-    if(rawStateName === 'REPARADO'){
-        text.msg1 = 'ya se encuentra reparado.'
-        text.msg2 = 'Esperamos su visita para que abone la reparación y retire el producto.'
-    }
-    if(rawStateName === 'CANCELADA'){
-        text.msg1 = 'ha sido retirado sin reparar.'
-        text.msg2 = 'No se ha aprobado la reparación y esta ha sido cancelada.'
-    }
-    if(rawStateName === 'ENTREGADO'){
-        text.msg1 = 'ha sido reparado.'
-        text.msg2 = 'La reparación fue abonada y el producto retirado.'
-    }
-    return text
 }
